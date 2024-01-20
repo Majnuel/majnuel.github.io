a3b43dff06bb3d8e73ce24da071d7cb388a59b45
Emmanuel Calle <emacalle@hotmail.com>
2024-01-20 14:48:46 +0100

    out dir not ignored

---
+++ out/_next/static/QV_Rm_e8aj4zMqY9yJIn-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/849-3716297df8716b44.js","static/css/c76082be1720afa1.css","static/chunks/pages/index-aebd3c810c2f8b69.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
